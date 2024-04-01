ae06390ae73ba7e0f718764e302dbf34aaf9f03c
tayna20 <106112536+tayna20@users.noreply.github.com>
2024-04-01 08:17:13 -0300

    Add files via upload

--- TERCEIRO ANO 1 TRIMESTRE/main.js
+++ TERCEIRO ANO 1 TRIMESTRE/main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
-botoes[i].onclick = function(){
+    botoes[i].onclick = function(){
 
     for(let j=0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo");
@@ -12,5 +12,9 @@ botoes[i].onclick = function(){
     botoes[i].classList.add('ativo');
     textos[i].classList.add('ativo');
 }
-console.log();
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05")
+
+contadores[0].textContent = "Contagem regressiva";
