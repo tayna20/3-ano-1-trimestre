a8581d7269b4e4c56603643d93332d70f6780eb4
tayna20 <106112536+tayna20@users.noreply.github.com>
2024-04-02 10:01:17 -0300

    Add files via upload

--- TERCEIRO ANO 1 TRIMESTRE/main.js
+++ TERCEIRO ANO 1 TRIMESTRE/main.js
@@ -15,6 +15,20 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05")
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
 
-contadores[0].textContent = "Contagem regressiva";
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    function calculaTempo(tempoObjetivo) {
+        // código omitido…
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+      }
+      
+  
